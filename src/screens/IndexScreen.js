9b96dfa584d0699db8f99af2d5f469c492da9ebf
Guilherme Silva <silva.gsg@gmail.com>
2023-09-15 17:53:41 +0100

    feat: Update with useReducer and automate Context creation

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,26 +1,48 @@
 import React, { useContext } from "react";
 import { View, Text, StyleSheet, FlatList, Button } from "react-native";
 
-import BlogContext from "../context/BlogContext";
+import { Feather } from "@expo/vector-icons";
+
+import { Context as BlogContext } from "../context/BlogContext";
 
 const IndexScreen = () => {
-  const { data, addBlogPost } = useContext(BlogContext);
+  const { state, addBlogPost } = useContext(BlogContext);
 
   return (
     <View>
-      <Text>Index Screen</Text>
+      {/* <Text>Index Screen</Text> */}
       <Button title="Add Post" onPress={addBlogPost} />
       <FlatList
-        data={data}
+        data={state}
         keyExtractor={(blogPosts) => blogPosts.title}
         renderItem={({ item }) => {
-          return <Text>{item.title}</Text>;
+          return (
+            <View style={styles.blogItem}>
+              <Text style={styles.blogTitle}>{item.title}</Text>
+              <Feather style={styles.deleteIcon} name="trash" />
+            </View>
+          );
         }}
       />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  blogItem: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingVertical: 20,
+    paddingHorizontal: 10,
+    borderTopWidth: 1,
+    borderColor: "grey",
+  },
+  blogTitle: {
+    fontSize: 18,
+  },
+  deleteIcon: {
+    fontSize: 24,
+  },
+});
 
 export default IndexScreen;
