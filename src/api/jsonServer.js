67fe2fc09b9485b8d8719c46cda6048b7e1bcaf1
Guilherme Silva <silva.gsg@gmail.com>
2023-11-10 11:57:43 +0000

    feat: Edit blogpost

--- src/api/jsonServer.js
+++ src/api/jsonServer.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://8549-109-49-61-34.ngrok-free.app",
+  baseURL: "https://ae96-2001-8a0-65ac-1900-7848-519b-f598-929b.ngrok-free.app",
 });
